ab37fba2d047fc116b3aedf11d2d755e3c419ba1
Dolev <dolev.gabay@norman-ai.com>
2024-12-13 17:54:24 +0200

    Initial commit for evernetix project

---
+++ server/server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+let todos = [
+    { id: 1, text: 'Cras justo odio', completed: true, category: 'work' },
+    { id: 2, text: 'Dapibus ac facilisis in', completed: true, category: 'personal' },
+    { id: 3, text: 'Morbi leo risus', completed: false, category: 'work' },
+    { id: 4, text: 'Porta ac consectetur ac', completed: false, category: 'personal' },
+    { id: 5, text: 'Vestibulum at eros', completed: false, category: 'work' },
+];
+
+let categories = ['work', 'personal', 'Uncategorized'];
+
+app.get('/todos', (req, res) => {
+    res.json(todos);
+});
+
+app.post('/todos', (req, res) => {
+    const { text, category } = req.body;
+    if (!text) return res.status(400).json({ message: 'Text is required' });
+    const newTodo = {
+        id: Date.now(),
+        text,
+        completed: false,
+        category,
+    };
+    todos.push(newTodo);
+    res.json(newTodo);
+});
+
+app.patch('/todos/:id', (req, res) => {
+    console.log(req.body);
+    const { id } = req.params;
+    const { text, completed } = req.body;
+    const todo = todos.find(todo => todo.id == id);
+    if (text) todo.text = text;
+    if (completed !== undefined) todo.completed = completed;
+    res.json(todo);
+});
+
+app.delete('/todos/:id', (req, res) => {
+    const { id } = req.params;
+    todos = todos.filter(todo => todo.id != id);
+    res.json({ id });
+});
+
+app.get('/categories', (req, res) => {
+    res.json(categories);
+});
+
+app.post('/categories', (req, res) => {
+    const body = req.body;
+    console.log(body.name);
+    const category = body.name;
+    if (!category) return res.status(400).json({ message: 'Category is required' });
+    categories.push(category);
+    res.json(category);
+});
+
+// Start server
+const PORT = 5001;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
